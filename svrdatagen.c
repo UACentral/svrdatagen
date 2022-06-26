c65144e0e0980808a6f143e5cad89e79c958a5d6
TukoTech <tuko.ustx@gmail.com>
2022-06-26 01:17:34 -0500

    Clean-up

--- svrdatagen.c
+++ svrdatagen.c
@@ -3,39 +3,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-UA_UInt32 uiCurrentValue = 10; 
-static UA_StatusCode
-readCurrentValue(UA_Server *server,
-                const UA_NodeId *sessionId, void *sessionContext,
-                const UA_NodeId *nodeId, void *nodeContext,
-                UA_Boolean sourceTimeStamp, const UA_NumericRange *range,
-                UA_DataValue *dataValue) {
-    UA_Variant_setScalarCopy(&dataValue->value, &uiCurrentValue,
-                             &UA_TYPES[UA_TYPES_UINT32]);
-
-    // UA_String out = UA_STRING_NULL;
-    // UA_String_copy(&nodeId->identifier.string, &out);
-    // // printf("%.*s\n", (int)out.length, out.data);
-    // int result;
-    // result = strcmp(out.data, "uint32.1");
-    // // printf("%d", result);
-    // if (0 == result ) {
-    //     uiCurrentValue++;
-    // }
-    // UA_String_clear(&out);
-    dataValue->hasValue = true;
-    return UA_STATUSCODE_GOOD;
-}
-
-static UA_StatusCode
-writeCurrentValue(UA_Server *server,
-                 const UA_NodeId *sessionId, void *sessionContext,
-                 const UA_NodeId *nodeId, void *nodeContext,
-                 const UA_NumericRange *range, const UA_DataValue *data) {
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
-                "Changing the current value is not implemented");
-    return UA_STATUSCODE_BADINTERNALERROR;
-}
+UA_UInt32 uiCurrentValue = 0; 
 
 static void
 beforeRead(UA_Server *server,
@@ -46,12 +14,6 @@ beforeRead(UA_Server *server,
     UA_UInt32 intValue = *(UA_UInt32 *)nodeContext;
     UA_Variant value;
     UA_Variant_setScalar(&value, &intValue, &UA_TYPES[UA_TYPES_UINT32]);
-    size_t len = (size_t)(*&nodeid->identifier.string.length);
-    char* convert = (char*)UA_malloc(sizeof(char)*(len)+1);
-    memcpy(convert, *&nodeid->identifier.string.data, len );
-    convert[len] = '\0';
-
-    const UA_NodeId ni = UA_NODEID_STRING(1, convert ); 
     UA_Server_writeValue(server, **&nodeid, value);
 }
 
@@ -60,7 +22,7 @@ addDataSourceVariable(UA_Server *server) {
     char nodeName[18];
     char snum[11];
 
-    for(int i = 1; i <= 10; i++) {
+    for(int i = 1; i <= 100000; i++) {
         strcpy(nodeName, "uint32.");
         sprintf(snum, "%d", i);
         strcat(nodeName, snum);
@@ -106,7 +68,7 @@ void *updateCurrentValue(void *arg)
         }
 
         UA_sleep_ms(1000);
-        printf("Updating: %d\n", uiCurrentValue);
+        // printf("Updating: %d\n", uiCurrentValue);
     }
     printf("Exiting updateCurrentValue\n");
 }
